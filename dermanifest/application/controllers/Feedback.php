eb32d25b0cbb7f498501b8af46dad265a4980c2a
muhammad anindyo <71876709+anindyoo@users.noreply.github.com>
2020-12-30 15:09:35 +0700

    Update Feedback.php

--- dermanifest/application/controllers/Feedback.php
+++ dermanifest/application/controllers/Feedback.php
@@ -25,4 +25,29 @@ class Feedback extends CI_Controller
         $this->load->view('feedback');
         $this->load->view('layout/footer', $footer);
     }
+    
+    public function simpan()
+    {
+        if ($this->input->post('submit-btn') != 'simpan')
+            redirect(base_url());
+
+        if ($this->fv->run('simpan-feedback')) {
+            $data = $this->input->post();
+            $data['idUser'] = $this->my_encrypt->decrypt($this->session->userdata('id'));
+            unset($data['submit-btn']);
+            $this->load->model('Feedback_model');
+            if ($this->Feedback_model->tambah($data)) {
+                $this->session->set_flashdata('status', 'success');
+                $this->session->set_flashdata('msg', 'Berhasil mengisi feedback');
+            } else {
+                $this->session->set_flashdata('status', 'gagal');
+                $this->session->set_flashdata('msg', 'Gagal mengisi feedback');
+            }
+        } else {
+            $this->session->set_flashdata('status', 'gagal');
+            $this->session->set_flashdata('msg', validation_errors());
+            var_dump(validation_errors());
+        }
+        redirect(base_url('feedback'));
+    }
 }
